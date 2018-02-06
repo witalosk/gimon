703ebab5d6cb709ace9763069ee7b901287e1a6d
Shun Nagatsu <shun_september@live.jp>
2018-02-06 14:25:48 +0900

    数字修正

--- classes/controller/GimonController.class.php
+++ classes/controller/GimonController.class.php
@@ -89,7 +89,7 @@ class GimonController extends ControllerBase
       Db::commit();
     }
 
-    $this->view->assign('all', 280 - mb_strwidth("Q: {$objGm->text}\nA: \n#gimon #gimon{$objUm->screen_name}\n",'UTF-8'));
+    $this->view->assign('all', 257 - mb_strwidth("Q: {$objGm->text}\nA: \n#gimon #gimon{$objUm->screen_name}\n",'UTF-8'));
     $this->view->assign('gimon', (array)$objGm);
     $this->view->assign('script', $script);
     $this->view->assign('answer', $answer);
